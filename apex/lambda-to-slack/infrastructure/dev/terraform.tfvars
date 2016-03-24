cdd6abd78392f4752d346629ed0b70cf4a3356cf
knakayama <knakayama.sh@gmail.com>
2016-03-24 18:35:28 +0900

    Add lambda to slack

---
+++ apex/lambda-to-slack/infrastructure/dev/terraform.tfvars
@@ -0,0 +1,9 @@
+name   = "lambda-to-slack"
+region = "ap-northeast-1"
+
+vpc_cidr      = "172.16.0.0/16"
+az            = "ap-northeast-1a"
+public_subnet = "172.16.0.0/24"
+
+ec2_instance_type   = "t2.micro"
+ec2_instance_ami_id = "ami-f80e0596"
---
+++ apex/lambda-to-slack/infrastructure/modules/compute/web/web.tf
@@ -0,0 +1,64 @@
+variable "name"             { default = "web" }
+variable "vpc_id"           { }
+variable "key_name"         { }
+variable "public_subnet_id" { }
+variable "instance_type"    { }
+variable "instance_ami_id"  { }
+
+resource "aws_security_group" "web" {
+  name        = "${var.name}"
+  vpc_id      = "${var.vpc_id}"
+  description = "Web SG"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = "${var.instance_ami_id}"
+  instance_type               = "${var.instance_type}"
+  vpc_security_group_ids      = ["${aws_security_group.web.id}"]
+  subnet_id                   = "${var.public_subnet_id}"
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 8
+  }
+
+  user_data = <<EOT
+#cloud-config
+repo_update: true
+repo_upgrade: all
+timezone: "Asia/Tokyo"
+
+packages:
+  - httpd
+
+runcmd:
+  - chkconfig httpd on
+  - service httpd start
+EOT
+}
+
+output "public_ip"   { value = "${aws_instance.web.public_ip}" }
+output "instance_id" { value = "${aws_instance.web.id}" }
