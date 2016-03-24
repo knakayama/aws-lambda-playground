ed50d3096266b3bd705f139e813cd0d6f0e6a6a5
knakayama <knakayama.sh@gmail.com>
2016-03-24 14:01:08 +0900

    Add python external module with apex

---
+++ apex/python-apex-external-module/functions/dependency/main.py
@@ -0,0 +1,20 @@
+import logging
+import requests
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handle(event, context):
+    logger.info("%s - %s", event, context)
+
+    url = "https://api.ipify.org?format=json"
+
+    raw = requests.get(url)
+    logger.info("%s", raw)
+    result = raw.json()
+
+    logger.info("Lambda IP: %s", result['ip'])
+
+    return event
