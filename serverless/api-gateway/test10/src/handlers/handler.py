762ba635b844589b2902b4c1d0bcc09b7b11ba82
knakayama <knakayama.sh@gmail.com>
2017-01-29 12:54:32 +0900

    Add test10

---
+++ serverless/api-gateway/test10/src/handlers/handler.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+import json
+
+
+def hello(event, context):
+    print(event)
+    response = {}
+    response['statusCode'] = 200
+    response['headers'] = {'x-custom-header': 'My Header Value'}
+    response['body'] = json.dumps({'event': event})
+
+    return response
