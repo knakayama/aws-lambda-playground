e2c98ae08bd53129a52d5dad5381499094928529
knakayama <knakayama.sh@gmail.com>
2017-03-21 15:46:43 +0900

    add

---
+++ serverless/warmup/handler.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+import json
+
+
+def hello(event, context):
+    if 'source' in event and event['source'] == 'serverless-plugin-warmup':
+        print(event)
+        return event
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
