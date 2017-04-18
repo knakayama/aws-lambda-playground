6fcf73cf4fb0c2be8bca248c1c510fd95dd8945d
knakayama <knakayama.sh@gmail.com>
2017-04-18 11:38:51 +0900

    Update

--- aws-sam/plural-events/src/handlers/func1/index.py
+++ aws-sam/plural-events/src/handlers/func1/index.py
@@ -1,6 +1,12 @@
 from __future__ import print_function
+from datetime import datetime
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'vendored'))
+import pytz
 
 
 def handler(event, context):
+    print(pytz.timezone('Asia/Tokyo').localize(datetime.now()))
     print(event)
     return event
