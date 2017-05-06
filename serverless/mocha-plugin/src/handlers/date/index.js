7ebebee15445f40b831659e46c029f16b1fc85cc
knakayama <knakayama.sh@gmail.com>
2017-05-06 11:00:57 +0900

    Add

---
+++ serverless/mocha-plugin/src/handlers/date/index.js
@@ -0,0 +1,13 @@
+exports.handler = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, the current date is ${new Date().toDateString()}.`,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
